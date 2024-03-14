4b9b881f77b2098f4aea17898fbde30de5c2d1c9
Rajat <130392640+rajatcantcode@users.noreply.github.com>
2024-03-15 02:55:00 +0530

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Rajat on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
