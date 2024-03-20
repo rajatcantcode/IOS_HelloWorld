21ece957aa7f5640c28916a6df2515e04958a79f
Rajat <130392640+rajatcantcode@users.noreply.github.com>
2024-03-21 04:49:11 +0530

    first commit

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -1,24 +1,15 @@
-//
-//  ContentView.swift
-//  HelloWorld
-//
-//  Created by Rajat on 15/03/24.
-//
-
 import SwiftUI
+
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello SwiftUI!")
     }
 }
+
 
 #Preview {
     ContentView()
 }
+
+
